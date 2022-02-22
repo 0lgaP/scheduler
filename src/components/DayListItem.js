ae9a7317592461a7cde675c8e94d9ddba7abc3b7
0lgatron <perronolga@gmail.com>
2022-02-22 15:59:23 -0500

    buttons, daylist - stories and css

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  
+  let dayClass = classNames( "day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
+  })
+
+  const formatSpots = (x) => {
+    let statement = "";
+    if(x === 0) {
+      statement += "no spots remaining";
+    }
+    if(x === 1) {
+      statement += "1 spot remaining";
+    } 
+    if(x > 1) {
+      statement += `${x} spots remaining`;
+    }
+    return statement;
+  }
+ 
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
