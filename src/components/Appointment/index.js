c0fafc251b92bf1f61dfe74d02f52dd81a69c5e3
0lgatron <perronolga@gmail.com>
2022-02-23 16:50:09 -0500

    Confirm, Empty, Error, Show, Status and their stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "components/Appointment/styles.scss";
-import Header from './Header';
+
 
 export default function Appointment(props) {
   const message = (props.time ? `Appointment at ${props.time}` : `No Appointments`)
