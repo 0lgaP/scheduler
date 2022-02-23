d688dd7f8727ed27e9f082ba4d497a9ee93561eb
0lgatron <perronolga@gmail.com>
2022-02-23 14:06:12 -0500

    interviewersList logic

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss";
+import classNames from 'classnames';
 
 export default function InterviewerList(props) {
-  
+  // const interviewerClass = classNames("interviewers", {
+  //   "interviwers__item--selected": props.selected
+  // })
+  const parsedInterviewerListItem = props.interviewers.map(interview => <InterviewerListItem 
+                                                                        key={interview.id} 
+                                                                        {...interview} 
+                                                                        setInterviewer={props.setInterviewer} 
+                                                                        selected={props.interviewer === interview.id}
+                                                                        />)
   return (
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list"></ul>
+    <ul className="interviewers__list">
+    {parsedInterviewerListItem}
+    </ul>
   </section>
   );
 }
