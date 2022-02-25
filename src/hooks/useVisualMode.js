d604c6499f9fcd297fa1201f402a796c25046ee4
0lgatron <perronolga@gmail.com>
2022-02-25 18:57:40 -0500

    first hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,36 @@
 import React, {useState} from 'react';
 
-export default function useVisualMode(initalMode){
-  const [mode, setMode] = useState(initalMode)
+export default function useVisualMode(firstMode){
+  const [mode, setMode] = useState(firstMode);
+  const [history, setHistory] = useState([firstMode]); 
+  //const setDay = day => setState({...state, day});
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+
+  const transition = (newMode, replace = false) => {
+    //set mode to variable
+    setMode(newMode);
+    //add mode to history array
+    if(replace){
+      setHistory((prev) => [ ...prev.slice(0, -1), newMode])
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
+    }
   
-  return {mode};
+  
+  const back = () => {
+    //set mode to the length of the array 
+    //-1 because array starts at 0, 
+    //-1 because we want to set it to the previous value in array
+    setMode(history[history.length - 2]);
+    //slice(start, end)
+    //(start at history[0], slice off history[-1])
+    setHistory((prev) => [...prev.slice(0, -1)]);
+  }
+
+  return {transition, back, mode};
+  
 };
+  
+  
+  
