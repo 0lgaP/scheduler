6d644da8e5a9b4540c49fdb738a41a1b63e57d4f
0lgatron <perronolga@gmail.com>
2022-02-25 15:34:22 -0500

    add react lib package and change state in test

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -43,17 +43,17 @@ const state = {
 };
 
 test("getAppointmentsForDay returns an array", () => {
-  const result = getAppointmentsForDay(state1, "Monday");
+  const result = getAppointmentsForDay(state, "Monday");
   expect(Array.isArray(result)).toBe(true);
 });
 
 test("getAppointmentsForDay returns an array with a length matching the number of appointments for that day", () => {
-  const result = getAppointmentsForDay(state1, "Monday");
+  const result = getAppointmentsForDay(state, "Monday");
   expect(result.length).toEqual(3);
 });
 
 test("getAppointmentsForDay returns an array containing the correct appointment objects", () => {
-  const [first, second] = getAppointmentsForDay(state1, "Tuesday");
+  const [first, second] = getAppointmentsForDay(state, "Tuesday");
   expect(first).toEqual(state.appointments["4"]);
   expect(second).toEqual(state.appointments["5"]);
 });
@@ -64,7 +64,7 @@ test("getAppointmentsForDay returns an empty array when the days data is empty",
 });
 
 test("getAppointmentsForDay returns an empty array when the day is not found", () => {
-  const result = getAppointmentsForDay(state1, "Wednesday");
+  const result = getAppointmentsForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
 
