1380582c5923516740a0390bd9c5d90997832c06
0lgatron <perronolga@gmail.com>
2022-03-01 18:11:20 -0500

    initial commit for form test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -15,13 +15,14 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
-
+  
  return(  
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
       <form autoComplete="off"
       >
         <input
+          data-testid="student-name-input"
           className="appointment__create-input text--semi-bold"
           name="name"
           type="text"
